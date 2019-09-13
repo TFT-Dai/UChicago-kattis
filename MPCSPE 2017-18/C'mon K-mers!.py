16587c0db40b0c2aa6cb0d37296f7e033d6eef48
bambi <37207287+TFT-Dai@users.noreply.github.com>
2019-09-13 10:55:53 +0800

    Create C'mon K-mers!.py

---
+++ MPCSPE 2017-18/C'mon K-mers!.py
@@ -0,0 +1,19 @@
+#https://uchicago.kattis.com/problems/uchicagoplacement.kmers
+
+L, N, k, Q = input().split(" ")
+s = ''
+for i in range(int(L)):
+    s += input()
+d = {}
+for i in range(len(s) - int(k) + 1):
+    substr = s[i:i+int(k)]
+    if substr in d:
+        d[substr] += 1
+    else:
+        d[substr] = 1
+for i in range(int(Q)):
+    q = input()
+    if q in d:
+        print(q, ' ', d[q])
+    else:
+        print(q, ' ', 0)
