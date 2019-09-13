7684ebc7052f9461fe46e4a7dbf908b1be26d4c4
bambi <37207287+TFT-Dai@users.noreply.github.com>
2019-09-13 10:47:40 +0800

    Create A Full-fill-ing Problem.py

---
+++ MPCSPE 2017-18/A Full-fill-ing Problem.py
@@ -0,0 +1,17 @@
+def fill(a, i, v, x):
+    if i < 0 or i >= len(a):
+        return
+    if a[i] != v:
+        return
+    if a[i] == x:
+        return
+    a[i] = x
+    fill(a, i-1, v, x)
+    fill(a, i+1, v, x)
+
+N, I, X = map(int, input().split(' '))
+arr = list(map(int, input().split(' ')))
+V = arr[I]
+fill(arr, I, V, X)
+for j in range(N):
+    print(arr[j], end=' ')
