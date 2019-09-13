c677c202be7e2de273b594d4a9b64557de6dd6e9
bambi <37207287+TFT-Dai@users.noreply.github.com>
2019-09-13 23:39:12 +0800

    Update Batter Up.py

--- MPCSPE 2017-18/Batter Up.py
+++ MPCSPE 2017-18/Batter Up.py
@@ -1,7 +1,7 @@
 #https://uchicago.kattis.com/problems/uchicagoplacement.batterup
 
 n = int(input())
-a = list(map(int, input().split(' ')))
+a = list(map(int, input().split()))
 sigma, count = 0, 0
 for item in a:
     if item >= 0:
